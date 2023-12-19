4bb8b20f5e55d1c36b3703156c9260fa4b8c80e4
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-12-19 23:44:02 +0900

    섹션 1: Hello World 섹션 2: 변수

---
+++ src/variable/Var5.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; //불리언 true, false
+        char d = 'A'; //문자 1개
+        String e = "Hello Java"; // 문자열
+
+        //코드에서 개발자가 직접 적은 100, 10.5, true, 'A', "Hello Java"와 같은 고정된 값을 프로그래밍 용어로 리터럴이라고 한다.
+    }
+}
